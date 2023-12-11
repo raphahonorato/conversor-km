cfebb6c35d5abaa2017e9fe9b7d878aa7c3cc391
raphahonorato <raphaahonorato@gmail.com>
2023-12-11 19:33:39 -0300

    adicionando a lógica

--- code/code.js
+++ code/code.js
@@ -1,3 +1,24 @@
-function calcular(){
-    
-}
+function calcular(event) {
+    event.preventDefault();
+
+    // Captura os valores dos inputs
+    var preco = parseFloat(document.getElementById("preco").value);
+    var km = parseFloat(document.getElementById("km").value);
+
+    // Verifica se os valores são válidos
+    if (!isNaN(preco) && !isNaN(km) && km !== 0) {
+        // Realiza a divisão
+        var resultadoDivisao = preco / km;
+
+        // Exibe o resultado na página
+        document.getElementById("resultado").innerHTML = resultadoDivisao.toFixed(2);
+    } else {
+        // Exibe uma mensagem de erro se os valores não forem válidos
+        document.getElementById("resultado").innerHTML = "Por favor, insira valores válidos e não divida por zero.";
+    }
+} 
+
+function selecionarTexto(input) {
+    // Seleciona todo o texto dentro do input
+    input.select();
+  }
